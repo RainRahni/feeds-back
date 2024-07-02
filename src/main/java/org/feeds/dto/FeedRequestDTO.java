c18afc5eaa5064aedf241175e5d587c065f5a1e4
rainr <rain.rahni@mail.ee>
2024-07-02 15:37:57 +0300

    add displaying description

--- src/main/java/org/feeds/dto/FeedRequestDTO.java
+++ src/main/java/org/feeds/dto/FeedRequestDTO.java
@@ -3,5 +3,5 @@ package org.feeds.dto;
 import lombok.Builder;
 
 @Builder
-public record FeedRequestDTO(Long id, String title, String link, String hexColor) {
+public record FeedRequestDTO(Long id, String title, String link, String hexColor, String description) {
 }
